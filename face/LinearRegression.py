aadebbfa71094a1a9271ac763414cf8a36d286b4
chenxiaoyu233 <2016060106020@std.uestc.edu.cn>
2018-04-06 18:08:21 +0800

    完成线性回归函数的编写

---
+++ face/LinearRegression.py
@@ -0,0 +1,28 @@
+#这个文件用于实现用多元线性回归
+
+import numpy as np
+from scipy import linalg
+
+def linearRegression(y, A):
+    """
+    输入:
+        y : numpy.ndarray
+        A : numpy.ndarray
+        需要保证y和X的行数相同(按照行向量的方式储存)
+        y和A中的每一个行向量都是一张素材
+    """
+    dim = A.shape
+    X = np.ones([1, dim[1]], dtype = 'float64')
+    X = np.append(X, A)
+    X = X.reshape([dim[0]+1, dim[1]])
+    b = linalg.inv(X.dot( X.transpose() )).dot(X.dot( y.transpose() ))
+    yPre = X.transpose().dot(b)
+    dy = y.transpose() - yPre
+    return dy.transpose().dot(dy)
+
+if __name__ == '__main__' : #测试
+    y = np.array([[15.8, 16.0, 15.9, 16.2, 16.5, 16.3, 16.8, 17.4, 17.2]])
+    A = np.array([[0, 1, 0, 1, 2, 0, 2, 3, 1],
+                  [0, 0, 1, 1, 0, 2, 2, 1, 3]])
+    linearRegression(y, A)
+
--- face/face.py
+++ face/face.py
@@ -1,6 +1,31 @@
+#scipy 库
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
+
+#本地库
+import PrincipalComponentsAnalisis as pca
+
+def classify(sample, dataBase, distance):
+    """
+    int classify(sample, dataBase)
+    用于判断sample到底归属于dataBase中的哪一个类别
+    这里的sample和dataBase都是已经降维过后的数据
+    输入:
+        sample : numpy.ndarray
+        dataBase : [numpy.ndarray] (注意到这个数据的外层套了一个list)
+        其中dataBase中的每一个ndarray表示了一个空间的基
+        distance : 距离函数(用于求sample到每个空间的距离)
+    输出:
+        返回一个整数, 表示sample属于的那个dataBase项的编号
+    """
+    dis = [distance(sample, item) for item in dataBase]
+    minx, minWhere = dis[0], 0
+    l = len(dis)
+    for i in range(l):
+        if dis[i] < minx : 
+            minx, minWhere = dis[i], i
+    return minWhere
 
 if __name__ == '__main__' : #程序入口
     pass
