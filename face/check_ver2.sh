287f2d7621e30fc3a126c1e054156b6f8cbd2230
chenxiaoyu233 <2016060106020@std.uestc.edu.cn>
2018-04-17 02:55:41 +0800

    添加了一组测试, 识别率69.9%

--- face/check_ver2.sh
+++ face/check_ver2.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-for name in ./att_faces/*
+for name in ./pic/*
 do
 	for pic in $name/*
 	do
 		mv $pic ./
 		expect=`basename $name`
 		filename=`basename $pic`
-		ans=`python3 face_ver2.py ./att_faces ./$filename fresh`
+		ans=`python3 face_ver2.py ./pic ./$filename fresh`
 		echo $expect $ans
 		mv ./$filename $pic
 	done
--- face/count.py
+++ face/count.py
@@ -1,10 +1,10 @@
+#coding=utf-8
 ans = 0
-total = 400
-for i in range(40):
-    for j in range(10):
-        line = input()
-        line = line.split(' ')
-        if line[0] == line[1]:
-            ans += 1
+total = 55
+for i in range(55):
+    line = input()
+    line = line.split(' ')
+    if line[0] == line[1]:
+        ans += 1
 
 print ('{}/{} = {}'.format(ans, total, ans/total))
