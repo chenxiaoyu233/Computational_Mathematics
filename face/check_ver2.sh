9c3a331e2e43320e240027f41b4eb4c94f0003b9
chenxiaoyu233 <2016060106020@std.uestc.edu.cn>
2018-04-17 01:45:13 +0800

    test

--- face/check_ver2.sh
+++ face/check_ver2.sh
@@ -7,7 +7,6 @@ do
 		mv $pic ./
 		expect=`basename $name`
 		filename=`basename $pic`
-		rm $name/.info
 		ans=`python3 face_ver2.py ./att_faces ./$filename fresh`
 		echo $expect $ans
 		mv ./$filename $pic
